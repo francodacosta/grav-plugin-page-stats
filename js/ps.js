9f6723fc3b387097b06754bd2e3763e4011a5e0c
Nuno Costa <nuno@francodacosta.com>
2022-08-28 11:26:23 +0100

    chore: collect stats on page load

--- js/ps.js
+++ js/ps.js
@@ -7,9 +7,13 @@ pageStats.createEvent = function(name, value){
 }
 
 console.log('PageStats::ping', {interval: pageStats.config.ping*1000})
-window.setInterval(function(){
+function time_on_page() {
     fetch(pageStats.url, {
         method: 'POST',
         body: JSON.stringify(pageStats.createEvent('ping', true))
     });
-}, pageStats.config.ping * 1000);
+}
+window.setInterval(time_on_page, pageStats.config.ping * 1000);
+
+//collect stats on page load
+time_on_page()
